dfa58d0db50a4b92d2d69a054b360aa3f52de8ad
Stener T. Nordnes <129489531+StenerNordnes@users.noreply.github.com>
2025-04-22 12:57:30 +0200

    Update ppi.h

--- 3_gpiote/ppi.h
+++ 3_gpiote/ppi.h
@@ -5,27 +5,19 @@
 #define PPI ((NRF_PPI_REG*)0x4001F000)
 
 typedef struct {
-	uint32_t EN;
-	uint32_t DIS;
-} PPI_TASKS_TYPE;
-
-typedef struct {
-	uint32_t EEP;
-	uint32_t TEP;
+	uint32_t EEP; // Event Endpoint
+	uint32_t TEP; // Task Endpoint
 } PPI_CHANNEL_TYPE;
 
 typedef struct {
-	// Tasks
-	volatile PPI_TASKS_TYPE PPI_TASKS[4];
-	// Registers
-	volatile uint32_t RESERVED0[312];
-	volatile uint32_t CHEN;
-	volatile uint32_t CHENSET;
-	volatile uint32_t CHENCLR;
+	volatile uint32_t RESERVED0[1284]; // 0x000 to 0x510
+	volatile uint32_t CHEN;       // 0x500
+	volatile uint32_t CHENSET;    // 0x504
+	volatile uint32_t CHENCLR;    // 0x508
 	volatile uint32_t RESERVED1;
-	volatile PPI_CHANNEL_TYPE PPI_CH[16];
+	volatile PPI_CHANNEL_TYPE PPI_CH[16]; // 0x510–0x58F
 	volatile uint32_t RESERVED2[156];
-	volatile uint32_t CHG[4];
+	volatile uint32_t CHG[4]; // 0x800–0x80C
 } NRF_PPI_REG;
 
 #endif
